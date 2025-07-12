2b610178ae94d7fc1eb5608ecc2b5bc024fa1158
Jacklyn Flag <jacklynf@at-l4.msk.21-school.ru>
2025-07-12 17:20:59 +0300

    Написать тесты для парсера опций

--- task10/cmd/main.go
+++ task10/cmd/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/pozedorum/WB_project_2.8/task10/internal/options"
+)
+
+func main() {
+	fs, args := options.ParceOptions()
+	fs.PrintFlags()
+	fmt.Println(args)
+}
--- task10/internal/options/options.go
+++ task10/internal/options/options.go
@@ -1,7 +1,10 @@
 package options
 
 import (
-	"flag"
+	"fmt"
+	"os"
+
+	flag "github.com/spf13/pflag"
 )
 
 type FlagStruct struct {
@@ -15,16 +18,38 @@ type FlagStruct struct {
 	HFlag *bool
 }
 
-func GetFlags() *FlagStruct {
+func ParceOptions() (*FlagStruct, []string) {
 	var fs FlagStruct
-	fs.KFlag = flag.Int("k", 0, "sort by column number N")
-	fs.NFlag = flag.Bool("n", false, "")
-	fs.RFlag = flag.Bool("r", false, "")
-	fs.UFlag = flag.Bool("u", false, "")
-	fs.MFlag = flag.Bool("m", false, "")
-	fs.BFlag = flag.Bool("b", false, "")
-	fs.CFlag = flag.Bool("c", false, "")
-	fs.HFlag = flag.Bool("h", false, "")
-	flag.Parse()
-	return &fs
+
+	flagset := flag.NewFlagSet("myflags", flag.ContinueOnError)
+	flagset.SetNormalizeFunc(func(f *flag.FlagSet, name string) flag.NormalizedName {
+		return flag.NormalizedName(name)
+	})
+
+	fs.KFlag = flagset.Int("k", 0, "sort by column number N")
+	fs.NFlag = flagset.Bool("n", false, "try to interpret strings as numbers and sort by it")
+	fs.RFlag = flagset.Bool("r", false, "sort in reverse order")
+	fs.UFlag = flagset.Bool("u", false, "output only sorted unique string")
+	fs.MFlag = flagset.Bool("m", false, "sort by month")
+	fs.BFlag = flagset.Bool("b", false, "ignore trailing blanks")
+	fs.CFlag = flagset.Bool("c", false, "check if data is sorted")
+	fs.HFlag = flagset.Bool("h", false, "sort by numerical value, taking into account suffixes (for example, K = kilobyte, M = megabyte).")
+
+	err := flagset.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+	return &fs, flagset.Args()
+}
+
+func (fs *FlagStruct) PrintFlags() {
+	fmt.Println("flag k -", *(fs.KFlag))
+	fmt.Println("flag n -", *(fs.NFlag))
+	fmt.Println("flag r -", *(fs.RFlag))
+	fmt.Println("flag u -", *(fs.UFlag))
+	fmt.Println("flag m -", *(fs.MFlag))
+	fmt.Println("flag b -", *(fs.BFlag))
+	fmt.Println("flag c -", *(fs.CFlag))
+	fmt.Println("flag h -", *(fs.HFlag))
 }
